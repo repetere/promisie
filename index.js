33fb85062481d7a2a4b5c11ddcf0ec965cee14ef
janbialostok <janbialostok@gmail.com>
2016-10-11 15:48:02 -0400

    updated to not incorrectly define rejection handler for chainable methods

--- index.js
+++ index.js
@@ -38,32 +38,38 @@ class Promisie extends Promise {
   constructor (options) {
     super(options);
     this.try = (onSuccess, onFailure) => {
-      return this.then(data => {
+      let success = function (data) {
         try {
           return (typeof onSuccess === 'function') ? onSuccess(data) : null;
         }
         catch (e) {
           return Promise.reject(e);
         }
-      }, e => (typeof onFailure === 'function') ? onFailure(e) : null);
+      };
+      let failure = (typeof onFailure === 'function') ? onFailure : undefined;
+      return this.then(success, failure);
     };
     this.spread = (onSuccess, onFailure) => {
-      return this.then(data => {
+      let success = function (data) {
         if (typeof data[Symbol.iterator] !== 'function') return Promise.reject(new TypeError('ERROR: spread expects input to be iterable'));
         if (typeof onSuccess !== 'function') return Promise.reject(new TypeError('ERROR: spread expects onSuccess handler to be a function'));
         return onSuccess(...data);
-      }, e => (typeof onFailure === 'function') ? onFailure(e) : null);
+      };
+      let failure = (typeof onFailure === 'function') ? onFailure : undefined;
+      return this.then(success, failure);
     };
     this.map = (onSuccess, onFailure, concurrency) => {
       if (typeof onFailure === 'number') {
         concurrency = onFailure;
         onFailure = undefined;
       }
-      return this.then(data => {
+      let success = function (data) {
         if (!Array.isArray(data)) return Promise.reject(new TypeError('ERROR: map expects input to be an array'));
         if (typeof onSuccess !== 'function') return Promise.reject(new TypeError('ERROR: map expects onSuccess handler to be a function'));
         return Promisie.map(data, concurrency, onSuccess);
-      }, e => (typeof onFailure === 'function') ? onFailure(e) : null);
+      };
+      let failure = (typeof onFailure === 'function') ? onFailure : undefined;
+      return this.then(success, failure);
     };
   }
 	static promisify (fn, _this) {
